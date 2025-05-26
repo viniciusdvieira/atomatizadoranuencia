497e802d8475532ea51c48e044264d9928b910c8
Vinicius Dias Vieira <130931338+viniciusdvieira@users.noreply.github.com>
2025-05-26 10:40:41 -0300

    add contrato

--- server.js
+++ server.js
@@ -116,6 +116,49 @@ app.post('/gerar-termo', async (req, res) => {
 
     res.send(buffer);
 });
+app.post('/gerar-contrato-estagio', async (req, res) => {
+    const {
+        nomeEstagiario, cpfEstagiario, ruaEstagiario, bairroEstagiario, cepEstagiario, cidadeEstagiario, curso, periodo,
+        nomeInstituicao, cnpjInstituicao, ruaInstituicao, bairroInstituicao, cepInstituicao, cidadeInstituicao,
+        nomeOrgao, cnpjOrgao, ruaOrgao, bairroOrgao, cepOrgao, cidadeOrgao, representanteOrgao
+    } = req.body;
+
+    const enderecoEstagiario = `${ruaEstagiario}, Bairro: ${bairroEstagiario}, CEP: ${cepEstagiario}, ${cidadeEstagiario}`;
+    const enderecoInstituicao = `${ruaInstituicao}, Bairro: ${bairroInstituicao}, CEP: ${cepInstituicao}, ${cidadeInstituicao}`;
+    const enderecoOrgao = `${ruaOrgao}, Bairro: ${bairroOrgao}, CEP: ${cepOrgao}, ${cidadeOrgao}`;
+
+    const doc = new Document({
+        sections: [{
+            properties: {
+                page: {
+                    margin: { top: 1440, bottom: 1080, left: 1440, right: 1440 }
+                }
+            },
+            children: [
+                new Paragraph({
+                    alignment: AlignmentType.JUSTIFIED,
+                    children: [
+                        new TextRun({
+                            text: `Pelo presente instrumento as partes abaixo discriminadas: Firmam entre si TERMO DE COMPROMISSO PARA A REALIZAÇÃO DE ESTÁGIO, regido pela Lei nº. 11.788, de 25 de setembro de 2008, e no que couber pelo Decreto estadual nº. 13.840, de 21 de setembro de 2009, segundo as seguintes cláusulas:\n\nCONCEDENTE: ${nomeOrgao}, CNPJ: ${cnpjOrgao}, com endereço na ${enderecoOrgao}, representada por ${representanteOrgao}.\n\nINTERVENIENTE: SECRETARIA DA ADMINISTRAÇÃO DO ESTADO DO PIAUÍ - SEAD, CNPJ: 06.553.481/0003-00, com endereço na Av. Pedro Freitas, S/N. BL. 01 – CENTRO ADMINISTRATIVO, Bairro São Pedro, em Teresina - PI, representada por SAMUEL PONTES DO NASCIMENTO.\n\nESTAGIÁRIO(A): ${nomeEstagiario}, brasileiro(a), CPF: ${cpfEstagiario}, residente e domiciliado na ${enderecoEstagiario}.\n\nINSTITUIÇÃO DE ENSINO: ${nomeInstituicao}, CNPJ: ${cnpjInstituicao}, com sede na ${enderecoInstituicao}, que subscreve esse ato através do seu representante legal (coordenador do curso, secretário acadêmico ou preposto).\n\nCLÁUSULA PRIMEIRA\nO estágio oferecido para discentes do curso de ${curso}, no qual o estudante está cursando o ${periodo} período em andamento, é regido por este Termo de Compromisso, visando propiciar ao estudante uma experiência acadêmico-profissional em um campo de trabalho determinado, visando:`,
+                            font: "Calibri",
+                            size: 28
+                        })
+                    ]
+                })
+            ]
+        }]
+    });
+
+    const buffer = await Packer.toBuffer(doc);
+    const nomeArquivo = `Contrato_Estagio_${nomeEstagiario.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/\s+/g, "_")}.docx`;
+
+    res.set({
+        'Content-Disposition': `attachment; filename="${nomeArquivo}"`,
+        'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    });
+
+    res.send(buffer);
+});
 
 
 app.listen(3000, () => {
